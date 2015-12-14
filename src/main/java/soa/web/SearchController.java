82308c58149f2b63738099f936c30a08ce81b61c
PhyrionX <rgcmb@hotmail.com>
2015-12-14 16:41:14 +0100

    Finish him!!!

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,22 +7,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class SearchController {
 
-	@Autowired
-	  private ProducerTemplate producerTemplate;
+    @Autowired
+    private ProducerTemplate producerTemplate;
 
-	@RequestMapping("/")
+    @RequestMapping("/")
     public String index() {
         return "index";
     }
 
 
-    @RequestMapping(value="/search")
+    @RequestMapping(value = "/search")
     @ResponseBody
-    public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+    public Object search(@RequestParam("q") String q, @RequestParam(value = "max", required = false) Integer max) {
+        //Necesitamos 2 cabeceras unas lo que queremos buscar y otra el máximo de tweets que queremos recuperar
+        Map<String, Object> headers = new HashMap<String, Object>();
+        //Consulta de búsqueda
+        headers.put("CamelTwitterKeywords", q);
+        //Límite
+        //Si no hay límite recupera 10 cómo máximo
+        headers.put("CamelTwitterCount", max);
+        return producerTemplate.requestBodyAndHeaders("direct:search", "", headers);
+
     }
 }
